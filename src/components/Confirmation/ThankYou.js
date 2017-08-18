6a87b30b1cf6d27a6cf39e72afb32a743adb85a8
Im Khem <im.khem@gmail.com>
2017-08-18 15:47:40 -0600

    Update to Confirmation Page

--- src/components/Confirmation/ThankYou.js
+++ src/components/Confirmation/ThankYou.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
+import { Grid } from 'react-bootstrap';
+
+import './ThankYou.css';
+
 class ThankYou extends React.Component {
+
   render(){
     return(
-      <h1>Order Confirmed</h1>
+      <Grid className="Confirmation">
+        <h1>Order Confirmed</h1>
+
+        <h3>{`${this.props.user.firstname} ${this.props.user.lastname}`}</h3>
+      </Grid>
     )
   }
 }
 
-export default ThankYou;
+function mapStateToProps(state){
+  return {
+    user: state.user,
+    cart: state.cart
+  }
+}
+
+export default connect(mapStateToProps)(ThankYou);
